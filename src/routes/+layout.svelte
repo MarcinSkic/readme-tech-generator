733c80394f54f5cc26266df99017fc2cc4032114
Marcin Skic <marcinskicoficjal@gmail.com>
2024-02-10 12:14:49 +0100

    ✨ Add footer
    🔧 Remove page link as there is only one page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,12 @@
-<script>
+<script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { page } from '$app/stores';
 	import '../app.pcss';
-	import { ModeWatcher, setMode } from 'mode-watcher';
+	import { ModeWatcher, setMode, mode } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { tech } from '$lib/tech';
 
-	const links = [
-		{
-			name: 'Tech list',
-			href: '/'
-		}
-	];
+	const links: { href: string; name: string }[] = [];
 	setMode('dark');
 </script>
 
@@ -30,3 +26,16 @@
 <main>
 	<slot />
 </main>
+<footer class="flex items-center justify-center gap-4 p-6 text-muted-foreground">
+	<span>Powered by <a href="https://iconify.design">Iconify.</a></span>
+	<span>Made by Marcin Skic</span>
+	<a href="https://github.com/MarcinSkic/readme-tech-generator">
+		<img
+			class="h-5"
+			src="https://api.iconify.design/simple-icons:github.svg?color=%23{$mode === 'light'
+				? '000000'
+				: 'ffffff'}"
+			alt="Github icon"
+		/></a
+	>
+</footer>
