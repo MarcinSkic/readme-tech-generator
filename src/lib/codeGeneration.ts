86cae657c73c8411ebb029de403251b3b1e01292
ft <filiptarajko@gmail.com>
2024-03-03 00:28:29 +0100

    ♻️ Replace reduce with for loop in codeGeneration

--- src/lib/codeGeneration.ts
+++ src/lib/codeGeneration.ts
@@ -1,11 +1,12 @@
 import type { TechInList } from './tech';
 
 export function getCode(selectedTechList: TechInList[], iconSize: number, header: string) {
-	return selectedTechList.reduce((text, tech) => {
+	let result = header+(header.length ? "\n\n" : "");
+	for (let i=0; i<selectedTechList.length; i++) {
+		let tech = selectedTechList[i];
 		const width = iconSize * (tech.dimensions?.widthRatio ?? 1);
 		const height = iconSize * (tech.dimensions?.heightRatio ?? 1);
 		let picture = '';
-
 		let alignment = tech.lastInGroup ? '' : ' align="left"';
 		let lineSpacing = tech.lastInGroup ? '\n\n' : '\n';
 
@@ -15,6 +16,7 @@ export function getCode(selectedTechList: TechInList[], iconSize: number, header
 			picture = `<picture><source media="(prefers-color-scheme: light)" srcset="${tech.image.dark}" /><img${alignment} width="${width}" height="${height}" alt="${tech.name}" src="${tech.image.light}" style="padding: 0 20px 16px 0"/></picture>`;
 		}
 
-		return (text += `[${picture}](${tech.webLink} "${tech.name}")${lineSpacing}`);
-	}, header+(header.length ? "\n\n" : ""));
+		result += `[${picture}](${tech.webLink} "${tech.name}")${lineSpacing}`;
+	}
+	return result;
 }
